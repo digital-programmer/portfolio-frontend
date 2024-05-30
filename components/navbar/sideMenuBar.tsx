f103741095bd2936bf00276de8716f2f0c7ffebe
diwakarKumarSapphire <155961073+diwakarKumarSapphire@users.noreply.github.com>
2024-05-30 13:37:24 +0530

    adjust sidebar menu size

--- components/navbar/sideMenuBar.tsx
+++ components/navbar/sideMenuBar.tsx
@@ -12,7 +12,7 @@ export default function SideMenuBar({ onBackdropClick }: {onBackdropClick: () =>
         <div className="fixed right-0 top-0 bottom-0 h-dvh backdrop-blur-sm w-full" onClick={
             () => onBackdropClick()
         }>
-            <div className="w-[50dvw] bg-white dark:bg-background-100 absolute right-0 top-0 bottom-0 h-dvh z-40 animate-apppearFromRight" onClick={handleNonBackdropClick}></div>
+            <div className="w-[60dvw] bg-white dark:bg-background-100 absolute right-0 top-0 bottom-0 h-dvh z-40 animate-apppearFromRight" onClick={handleNonBackdropClick}></div>
         </div>
     </>
 }
