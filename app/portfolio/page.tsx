4a972123bddc5bb703449d7dae289e7c07ecbb0a
diwakarKumarSapphire <155961073+diwakarKumarSapphire@users.noreply.github.com>
2024-06-01 17:18:00 +0530

    add portfolio

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,7 @@
+import Portfolio from "@/components/portfolio/portfolio";
+
+export default function PortfolioPage() {
+    return (
+      <Portfolio/>
+    );
+}
---
+++ components/portfolio/portfolio.tsx
@@ -0,0 +1,7 @@
+export default function Portfolio() {
+    return (
+      <div className="text-text-900 dark:text-text-900">
+        Portfolio Page
+      </div>
+    );
+  }
