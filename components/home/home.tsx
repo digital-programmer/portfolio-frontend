499dc44f0d8e50bfb0dfd6cd0db50d5b9de68c12
diwakarKumarSapphire <155961073+diwakarKumarSapphire@users.noreply.github.com>
2024-06-02 18:34:23 +0530

    add animationg

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
           <Link href={"/"} className="w-max inline-block">
             <button className="relative inline-flex overflow-hidden rounded-2xl p-[2px] focus:outline-none outline-none">
-              <span className="absolute inset-[-1000%] animate-[spin_4s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#f6baa2_0%,#f29773_50%,#fbdcd0_100%)]" />
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#f6baa2_0%,#f29773_50%,#fbdcd0_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-2xl bg-white dark:bg-background-100 px-4 py-2 md:px-6 md:py-2.5 font-medium backdrop-blur-3xl text-text-950 dark:text-text-950 text-[12px] md:text-sm">
                 Download Resume
               </span>
