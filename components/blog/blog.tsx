55aa69407994be81cdf8c14422e084f1f5dcad01
diwakarKumarSapphire <155961073+diwakarKumarSapphire@users.noreply.github.com>
2024-06-11 12:48:50 +0530

    show blog pitch

--- components/blog/blog.tsx
+++ components/blog/blog.tsx
@@ -2,18 +2,73 @@ import fetchBlogs from "@/actions/blog";
 import BlogList from "./blogList";
 
 import { DM_Sans } from "next/font/google";
+import { FaBook, FaDatabase } from "react-icons/fa6";
+import { MdSettingsSystemDaydream } from "react-icons/md";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export default async function Blog() {
   const blogs = await fetchBlogs();
   return (
-    <div>
-      <div className={"text-text-900 dark:text-text-900 text-xl md:text-3xl font-bold " + dmSans.className}>Latest Blogs</div>
-      <div className="mt-8 mb-2">
-        <BlogList blogs={blogs}/>
+    <div className="flex flex-col md:pt-4 gap-8 md:gap-16">
+      {/* start of blog pitch */}
+      <div className="flex flex-col md:items-center md:justify-center mb-8 md:mb-12 bg-white py-4 md:py-12 px-3 md:px-4 rounded-md dark:bg-background-50 dark:border dark:border-primary-200 shadow-inner gap-2">
+        <div
+          className={
+            "text-xl md:text-3xl font-semibold text-text-950 dark:text-text-950 " +
+            dmSans.className
+          }
+        >
+          Explore tech blog
+        </div>
+        <div className="font-light text-text-900 dark:text-text-900 text-[12px] md:text-sm mt-2 mb-4 md:text-center leading-4">
+          Dive into my thoughts, tutorials, and insights on software engineering, data science, web development, and technology.
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex items-center gap-1 md:gap-1.5 border border-primary-100 dark:border-primary-200 rounded-md py-1 px-2 justify-center shadow-inner">
+            <FaBook
+              className="text-accent-500 dark:text-accent-500"
+              size={16}
+            />
+            <div className="text-[9px] md:text-[12px] leading-[10px] md:leading-3 font-light text-text-950 dark:text-text-950 text-center">
+              Low Level Design
+            </div>
+          </div>
+          <div className="flex items-center gap-1 md:gap-1.5 border border-primary-100 dark:border-primary-200 rounded-md py-1 px-2 justify-center shadow-inner">
+            <FaDatabase
+              className="text-accent-500 dark:text-accent-500"
+              size={16}
+            />
+            <div className="text-[9px] md:text-[12px] leading-[10px] md:leading-3 font-light text-text-950 dark:text-text-950 text-center">
+              High Level Design
+            </div>
+          </div>
+          <div className="flex items-center gap-1 md:gap-1.5 border border-primary-100 dark:border-primary-200 rounded-md py-1 px-2 justify-center shadow-inner">
+            <MdSettingsSystemDaydream
+              className="text-accent-500 dark:text-accent-500"
+              size={16}
+            />
+            <div className="text-[9px] md:text-[12px] leading-[10px] md:leading-3 font-light text-text-950 dark:text-text-950 text-center">
+              
+              Core Concepts
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* end of blog pitch */}
+      <div>
+        <div
+          className={
+            "text-text-900 dark:text-text-900 text-xl md:text-3xl font-bold " +
+            dmSans.className
+          }
+        >
+          Latest Blogs
+        </div>
+        <div className="mt-8 mb-2">
+          <BlogList blogs={blogs} />
+        </div>
       </div>
     </div>
   );
 }
-
