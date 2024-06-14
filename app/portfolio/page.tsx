debbd5c389817110f4ab524a0af30a0d442b3050
diwakarKumarSapphire <155961073+diwakarKumarSapphire@users.noreply.github.com>
2024-06-15 01:05:36 +0530

    TODO:add projects

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,8 @@
 import Portfolio from "@/components/portfolio/portfolio";
 
 export default function PortfolioPage() {
+  // for portfolio page
+  // TODO: Add projects
     return (
       <Portfolio/>
     );
