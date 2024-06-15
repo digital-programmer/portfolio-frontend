e602a58c3349ad6908da07f309cbb1666be3bf23
digital-programmer <mohandibya123@gmail.com>
2024-06-15 12:33:39 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./global_icons.css";
-import Footer from "@/components/footer/footer"
+import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 import ThemeProvider from "@/utils/themeContext";
-import type { Metadata } from "next";  
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { GoogleAnalytics } from '@next/third-parties/google'
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import type { Metadata } from "next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,33 +23,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
       <body
-        className={
-          inter.className +
-          " bg-background-50 dark:bg-background-50"
-        }
+        className={inter.className + " bg-background-50 dark:bg-background-50"}
       >
         <ThemeProvider>
           <Navbar />
           <main className="lg:max-w-screen-md lg:mx-auto w-full text-text-900 dark:text-text-900 text-sm md:text-base">
             <div className="h-max flex flex-col p-4 md:p-8">
               {children}
-              <ToastContainer className="text-sm"/>
+              <ToastContainer className="text-sm" />
             </div>
           </main>
-          <Footer/>
+          <Footer />
         </ThemeProvider>
         {/* start of Gtag */}
         <GoogleAnalytics gaId="G-L5W36PRZ7J" />
         {/* end of Gtag */}
+
         {/* start of page insights */}
-        <SpeedInsights/>
+        <SpeedInsights />
         {/* end of page insights */}
+        
+        {/* start of vercel analytics */}
+        <Analytics />
+        {/* end of vercel analytics */}
       </body>
-      
     </html>
   );
 }
